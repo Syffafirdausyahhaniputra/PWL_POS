eff02d347c769a77e7d40eba0e8a835cf6eb4b40
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-09-18 08:10:26 +0700

    Js 4 Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,19 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        // // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data); // update data user
-        
         // coba akses model UserModel
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort(404);
-        }); // ambil semua data dari tabel m_user
+        $user = UserModel::where('username', 'manager9')->firstOrFail(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }   
 }
