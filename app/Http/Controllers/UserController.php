0de1d098925b2e2ce92022fe869d8257bbf8dddf
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-09-25 17:48:36 +0700

    Js 5 Praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,9 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     //Ambil data user dalam bentuk json untuk datatables//
@@ -32,14 +34,20 @@ class UserController extends Controller
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
+        
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+        
         return DataTables::of($users)
-        //menambahkan kolom index / no urut
-        ->addIndexColumn()
+        ->addIndexColumn() //menambahkan kolom index / no urut
         ->addColumn('aksi', function ($user) {
             $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a>';
             $btn .= '<a href="'.url('/user/' . $user->user_id. '/edit').'" class="btn btn-warning btn-sm">Edit</a>';
-            $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">'. csrf_field() . method_field('DELETE') .'<button type="submit" class="btn btn-danger btn-sm" onclick="return 
-confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>'; 
+            $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">'. 
+            csrf_field() . method_field('DELETE') .
+            '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>'; 
             return $btn;    
         })
         ->rawColumns(['aksi']) //memberitahu bahwa kolom aksi adalah html
